eed7caa573fc3831392f28a0558048a20145235d
eri <eriron1004@gmail.com>
2026-01-04 17:15:23 +0900

    step150_test1

---
+++ example.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+def greet():
+    print("こんにちは")
+
+greet()
+
+def print_name(name):
+    print(f"私の名前は{name}です")
+
+print_name("hello")
+
+def get_greet():
+    return print("おはようございます")
+
+get_greet()
+
+def add(a, b):
+    return print(a+b)
+
+add(3, 5)
